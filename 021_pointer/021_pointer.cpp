1a3b3e82e32d51d0d53de6ff2ef900cfda63d809
FirdausyTama <atama.firdausy@gmail.com>
2023-05-10 15:15:04 +0700

    untuk di gunakan membuat pointer reference ke ref mahasiswa dengan nim 2

--- 021_pointer/021_pointer.cpp
+++ 021_pointer/021_pointer.cpp
@@ -15,4 +15,8 @@ int main()
 {
 	mahasiswa mhs{ 1 };		//object mahasiswa
 	mhs.showNim();			//member acces operator
+
+	mahasiswa ref = mhs;	//pointer reference refmhs
+	ref.nim = 2;			//member acces operator
+	mhs.showNim();
 }
